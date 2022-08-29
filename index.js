2b1a90ab11ebaffd6ffd39df9598bdc7995f9d52
herryam <herryanugerah1@gmail.com>
2022-08-29 11:31:05 +0700

    First Commit

--- index.js
+++ index.js
@@ -10,14 +10,8 @@ const corsOptions = {
 app.get('/', cors(corsOptions), (req, res) => {
     res.json([
         {
-            "id": 1,
-            "name": "Herry",
-            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ac magna maximus, porta nibh eu, placerat velit. Maecenas ac malesuada purus. Curabitur at enim nisi. Etiam ornare laoreet dui id porta. Integer at nisl erat. Proin at ipsum nulla. Aenean gravida imperdiet augue, a egestas tellus porttitor ac. Praesent convallis ullamcorper dui, et vulputate metus. Nullam faucibus erat sapien, quis aliquet massa pharetra quis. Sed sagittis ipsum nisi, id porttitor augue imperdiet in. Vivamus ac diam sed dui pretium facilisis."
-        },
-        {
-            "id": 2,
-            "name": "Sasuke",
-            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ac magna maximus, porta nibh eu, placerat velit. Maecenas ac malesuada purus. Curabitur at enim nisi. Etiam ornare laoreet dui id porta. Integer at nisl erat. Proin at ipsum nulla. Aenean gravida imperdiet augue, a egestas tellus porttitor ac. Praesent convallis ullamcorper dui, et vulputate metus. Nullam faucibus erat sapien, quis aliquet massa pharetra quis. Sed sagittis ipsum nisi, id porttitor augue imperdiet in. Vivamus ac diam sed dui pretium facilisis."
+            "user": "Herry",
+            "pass": "whoami"
         }
     ]);
 });
